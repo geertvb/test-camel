5b8cad0cc9f832112792128e4955198b2a2a8d13
Geert Van Bastelaere <geert.van.bastelaere@gmail.com>
2022-07-09 13:37:14 +0200

    Simple grouped aggregation

--- src/main/java/eu/europa/ec/test/testcamelseda/MyRoute.java
+++ src/main/java/eu/europa/ec/test/testcamelseda/MyRoute.java
@@ -3,6 +3,7 @@ package eu.europa.ec.test.testcamelseda;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.aggregate.GroupedMessageAggregationStrategy;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -20,27 +21,33 @@ public class MyRoute extends RouteBuilder {
     @Override
     public void configure() {
         from("direct:start")
+                .setProperty("correlationId", simple("${id}"))
                 .wireTap("seda:tap")
                 .to("log:result");
 
         from("seda:tap")
                 .process(sleep("tap", 3000L))
                 .multicast()
-                    .to("direct:a")
-                    .to("direct:b")
+                .to("direct:a")
+                .to("direct:b")
                 .end()
                 .setBody().constant("Tapped")
-                .to("log:tap");
+                .to("direct:result");
 
         from("direct:a")
                 .process(sleep("a", 1000L))
                 .setBody().constant("a")
-                .to("log:a");
+                .to("direct:result");
 
         from("direct:b")
                 .process(sleep("b", 1000L))
                 .setBody().constant("b")
-                .to("log:b");
+                .to("direct:result");
+
+        from("direct:result")
+                .aggregate(new GroupedMessageAggregationStrategy())
+                .simple("${exchangeProperty.correlationId}")
+                .completionTimeout(10000L).to("log:result");
     }
 
 }
