87647cd3406223ff10b1f000483bac2b9cd134e5
Geert Van Bastelaere <geert.van.bastelaere@gmail.com>
2022-07-11 01:33:57 +0200

    Custom predicate

--- src/main/java/eu/europa/ec/test/testcamelseda/MyRoute.java
+++ src/main/java/eu/europa/ec/test/testcamelseda/MyRoute.java
@@ -1,10 +1,17 @@
 package eu.europa.ec.test.testcamelseda;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Predicate;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.processor.aggregate.GroupedMessageAggregationStrategy;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static org.apache.camel.ExchangePropertyKey.GROUPED_EXCHANGE;
 
 @Slf4j
 @Component
@@ -15,6 +22,24 @@ public class MyRoute extends RouteBuilder {
             log.info("Sleep {}", label);
             Thread.sleep(millis);
             log.info("Wake {}", label);
+        };
+    }
+
+    public Predicate finished(String... routes) {
+        return exchange -> {
+            List<Message> messages = exchange.getProperty(GROUPED_EXCHANGE, List.class);
+            List<String> bodies = messages.stream()
+                    .map(Message::getExchange)
+                    .map(Exchange::getIn)
+                    .map(Message::getBody)
+                    .map(String.class::cast)
+                    .toList();
+            for (String route : routes) {
+                if (!bodies.contains(route)) {
+                    return false;
+                }
+            }
+            return true;
         };
     }
 
@@ -47,7 +72,8 @@ public class MyRoute extends RouteBuilder {
         from("direct:result")
                 .aggregate(new GroupedMessageAggregationStrategy())
                 .simple("${exchangeProperty.correlationId}")
-                .completionSize(3)
+                //.completionSize(3)
+                .completionPredicate(finished("Tapped"))
                 .completionTimeout(10000L).to("log:result");
     }
 
